99f98dd8e0642d79713487c6ee847db830b696b6
stassia <stassiar@yahoo.com>
2016-10-18 03:32:38 +0000

    Add css stylesheet

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,8 +4,8 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
       def show
-      id = params[:id] # retrieve movie ID from URI route
-      @movie = Movie.find(id) # look up movie by unique ID
-      # will render app/views/movies/show.html.haml by default
+          id = params[:id] # retrieve movie ID from URI route
+          @movie = Movie.find(id) # look up movie by unique ID
+          # will render app/views/movies/show.html.haml by default
       end
 end
