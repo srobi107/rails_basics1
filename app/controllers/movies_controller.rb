24aade2a79cca4d2a12cc274ccc034ef8e8d5843
stassia <stassiar@yahoo.com>
2016-10-18 04:22:16 +0000

    having problems with NameError: uninitialized constant

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,9 +3,13 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-      def show
-          id = params[:id] # retrieve movie ID from URI route
-          @movie = Movie.find(id) # look up movie by unique ID
-          # will render app/views/movies/show.html.haml by default
-      end
+  def show
+      id = params[:id] # retrieve movie ID from URI route
+      @movie = Movie.find(id) # look up movie by unique ID
+      # will render app/views/movies/show.html.haml by default
+  end
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end 
 end
