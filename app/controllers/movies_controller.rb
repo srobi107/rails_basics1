c72b486b3b028ccdb91329e8838513753d67434c
stassia <stassiar@yahoo.com>
2016-10-21 06:59:56 +0000

    Complete the create functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,13 +3,34 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end 
+
+  def edit
+    @movie =Movie.find params[:id]
+  end
   def show
       id = params[:id] # retrieve movie ID from URI route
       @movie = Movie.find(id) # look up movie by unique ID
       # will render app/views/movies/show.html.haml by default
   end
-  def new
-    @movie = Movie.new
-    # default: render 'new' template
-  end 
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(user_params)
+    flash[:notice] = "# {@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  def create
+     @movie = Movie.create!(user_params)
+     flash[:notice] = " # {@movie.title} was successfully created."
+     redirect_to movies_path
+  end
+  private
+    def user_params
+     params.require(:movie)
+     params[:movie].permit(:title,:rating,:release_date)
+    end
+ 
 end
